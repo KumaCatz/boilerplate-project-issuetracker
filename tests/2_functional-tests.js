2e3b7fc486a6835b98be694bb55b34bbd27b0452
KumaCatz <tod.kagan@gmail.com>
2024-07-02 20:34:42 -0300

    test: formatting tests

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -6,5 +6,171 @@ const server = require('../server');
 chai.use(chaiHttp);
 
 suite('Functional Tests', function() {
-  
+    test('Create an issue with every field: POST request to /api/issues/{project}', function (done) {
+        chai
+          .request(server)
+          .get('/api/issues/{project}')
+          .query({})
+          .end((err, res) => {
+            if (err) return done(err);
+            assert.equal(res.body, {});
+            done();
+          });
+      });
+
+    //   test('Create an issue with only required fields: POST request to /api/issues/{project}', function (done) {
+    //     chai
+    //       .request(server)
+    //       .get('/api/issues/{project}')
+    //       .query({})
+    //       .end((err, res) => {
+    //         if (err) return done(err);
+    //         assert.equal(res.body, {});
+    //         done();
+    //       });
+    //   });
+
+    //   test('Create an issue with missing required fields: POST request to /api/issues/{project}', function (done) {
+    //     chai
+    //       .request(server)
+    //       .get('/api/issues/{project}')
+    //       .query({})
+    //       .end((err, res) => {
+    //         if (err) return done(err);
+    //         assert.equal(res.body, {});
+    //         done();
+    //       });
+    //   });
+
+    //   test('View issues on a project: GET request to /api/issues/{project}', function (done) {
+    //     chai
+    //       .request(server)
+    //       .get('/api/issues/{project}')
+    //       .query({})
+    //       .end((err, res) => {
+    //         if (err) return done(err);
+    //         assert.equal(res.body, {});
+    //         done();
+    //       });
+    //   });
+
+    //   test('View issues on a project with one filter: GET request to /api/issues/{project}', function (done) {
+    //     chai
+    //       .request(server)
+    //       .get('/api/issues/{project}')
+    //       .query({})
+    //       .end((err, res) => {
+    //         if (err) return done(err);
+    //         assert.equal(res.body, {});
+    //         done();
+    //       });
+    //   });
+
+    //   test('View issues on a project with multiple filters: GET request to /api/issues/{project}', function (done) {
+    //     chai
+    //       .request(server)
+    //       .get('/api/issues/{project}')
+    //       .query({})
+    //       .end((err, res) => {
+    //         if (err) return done(err);
+    //         assert.equal(res.body, {});
+    //         done();
+    //       });
+    //   });
+
+    //   test('Update one field on an issue: PUT request to /api/issues/{project}', function (done) {
+    //     chai
+    //       .request(server)
+    //       .get('/api/issues/{project}')
+    //       .query({})
+    //       .end((err, res) => {
+    //         if (err) return done(err);
+    //         assert.equal(res.body, {});
+    //         done();
+    //       });
+    //   });
+
+    //   test('Update multiple fields on an issue: PUT request to /api/issues/{project}', function (done) {
+    //     chai
+    //       .request(server)
+    //       .get('/api/issues/{project}')
+    //       .query({})
+    //       .end((err, res) => {
+    //         if (err) return done(err);
+    //         assert.equal(res.body, {});
+    //         done();
+    //       });
+    //   });
+
+    //   test('Update an issue with missing _id: PUT request to /api/issues/{project}', function (done) {
+    //     chai
+    //       .request(server)
+    //       .get('/api/issues/{project}')
+    //       .query({})
+    //       .end((err, res) => {
+    //         if (err) return done(err);
+    //         assert.equal(res.body, {});
+    //         done();
+    //       });
+    //   });
+
+    //   test('Update an issue with no fields to update: PUT request to /api/issues/{project}', function (done) {
+    //     chai
+    //       .request(server)
+    //       .get('/api/issues/{project}')
+    //       .query({})
+    //       .end((err, res) => {
+    //         if (err) return done(err);
+    //         assert.equal(res.body, {});
+    //         done();
+    //       });
+    //   });
+
+    //   test('Update an issue with an invalid _id: PUT request to /api/issues/{project}', function (done) {
+    //     chai
+    //       .request(server)
+    //       .get('/api/issues/{project}')
+    //       .query({})
+    //       .end((err, res) => {
+    //         if (err) return done(err);
+    //         assert.equal(res.body, {});
+    //         done();
+    //       });
+    //   });
+
+    //   test('Delete an issue: DELETE request to /api/issues/{project}', function (done) {
+    //     chai
+    //       .request(server)
+    //       .get('/api/issues/{project}')
+    //       .query({})
+    //       .end((err, res) => {
+    //         if (err) return done(err);
+    //         assert.equal(res.body, {});
+    //         done();
+    //       });
+    //   });
+
+    //   test('Delete an issue with an invalid _id: DELETE request to /api/issues/{project}', function (done) {
+    //     chai
+    //       .request(server)
+    //       .get('/api/issues/{project}')
+    //       .query({})
+    //       .end((err, res) => {
+    //         if (err) return done(err);
+    //         assert.equal(res.body, {});
+    //         done();
+    //       });
+    //   });
+
+    //   test('Delete an issue with missing _id: DELETE request to /api/issues/{project}', function (done) {
+    //     chai
+    //       .request(server)
+    //       .get('/api/issues/{project}')
+    //       .query({})
+    //       .end((err, res) => {
+    //         if (err) return done(err);
+    //         assert.equal(res.body, {});
+    //         done();
+    //       });
+    //   });
 });
